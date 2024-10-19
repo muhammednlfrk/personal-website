488ee4274ccb01a4b217117597faebad46ca4eea
Muhammed Nilifırka <41184896+muhammednlfrk@users.noreply.github.com>
2024-10-19 15:32:49 +0300

    Add PersonalWebsite.WebUI

---
+++ src/PersonalWebsite.WebUI/Program.cs
@@ -0,0 +1,22 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // TODO: Add exception handdler.
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
